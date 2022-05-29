3f07ad6133f3471a8756082c2c0c23d5cac0fb48
rootership <2486420157@qq.com>
2022-05-29 16:30:39 +0800

    API服务类

---
+++ app/src/main/java/com/example/imagerecogniton/network/APIService.java
@@ -0,0 +1,38 @@
+package com.example.imagerecogniton.network;
+
+/**
+ * @Author:rooterShip
+ * @Date:2022/5/29
+ * @LastEditors:rooterShip
+ * @LastEditTime:2022/5/29
+ */
+
+import com.example.imagerecogniton.model.GetTokenResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+/**
+ * API服务
+ */
+public interface APIService {
+    /**
+     * 获取鉴权认证Token
+     * 使用post请求需要带的三个url参数
+     * @param grant_type 类型
+     * @param client_id API key
+     * @param client_secret Secret Key
+     * @return GetTokenResponse
+     */
+
+    //表示请求体是一个Form表单
+    @FormUrlEncoded
+    //接受的字符串表示接口path，与baseUrl组成完成的Url
+    @POST("/oauth/2.0/token")
+    //@Field 表示表单字段
+    Call<GetTokenResponse> getToken(@Field("grant_type") String grant_type,
+                                    @Field("client_id") String client_id,
+                                    @Field("client_secret") String client_secret);
+}

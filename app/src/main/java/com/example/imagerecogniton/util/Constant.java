3944774ac4557873e6d763ed7fcf5e884e90b093
rootership <2486420157@qq.com>
2022-05-29 19:35:13 +0800

    全局变量工具类（标明token返回数据）

---
+++ app/src/main/java/com/example/imagerecogniton/util/Constant.java
@@ -0,0 +1,28 @@
+package com.example.imagerecogniton.util;
+
+/**
+ * @Author:rooterShip
+ * @Date:2022/5/29
+ * @LastEditors:rooterShip
+ * @LastEditTime:2022/5/29
+ */
+
+/**
+ * 全局变量
+ */
+public class Constant {
+    /**
+     * 鉴权Token
+     */
+    public static final String Token = "accessToken";
+
+    /**
+     * 获取的Token的时间
+     */
+    public static final String GET_TOKEN_TIME = "getTokenTime";
+
+    /**
+     * Token有效期
+     */
+    public static final String TOKEN_VALID_PERIOD = "tokenValidPeriod";
+}

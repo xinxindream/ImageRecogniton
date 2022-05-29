11944f3c1ece173ab65e82c28e80d10af3a16057
xinxindream <1877329715@qq.com>
2022-05-29 13:22:08 +0800

    增加RecycleView结果列表适配器，但是有些许问题，缺少GetDiscern类

---
+++ app/src/main/java/com/example/imagerecogniton/adapter/DiscernResultAdapter.java
@@ -0,0 +1,25 @@
+package com.example.imagerecogniton.adapter;
+
+
+import androidx.annotation.Nullable;
+
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.example.imagerecogniton.R;
+
+/**
+ * 增加RecycleView结果列表适配器
+ *
+ * @author: 潘孝飞
+ * */
+public class DiscernResultAdapter extends BaseQuickAdapter<GetDiscernResultResponse.ResultBean, BaseViewHolder> {
+    public DiscernResultAdapter(int layoutResId, @Nullable List<GetDiscernResultResponse.ResultBean> data) {
+        super(layoutResId, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, GetDiscernResultResponse.ResultBean item) {
+        helper.setText(R.id.tv_keyword,item.getKeyword())
+                .setText(R.id.tv_root,item.getRoot())
+                .setText(R.id.tv_score,String.valueOf(item.getScore()));
+    }
+}
